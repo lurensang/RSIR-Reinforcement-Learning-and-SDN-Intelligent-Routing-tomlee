eb037226437547b829aa9c7a6bdd607822e4c481
“luren” <liyunxiang126@126.com>
2023-08-14 21:56:13 +0800

    add comments

--- SDNapps_proac/RoutingGeant/get_R_Q.py
+++ SDNapps_proac/RoutingGeant/get_R_Q.py
@@ -10,25 +10,31 @@ def rand_number(seed):
 def gaussian(x, mu, sig):
     return round(np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.))), 3)
 
-def initial_R(A,Z,weight,A_Z_dict):
-    #input net is
+def initial_R(A, Z, weight, A_Z_dict):
+    # input net is
     R = {}
     net = copy.deepcopy(A_Z_dict)
+    # creat empty dictionary for reward
     for i in net.keys():
         sub_key = net[i]
         sub_dic = {}
         for j in sub_key:
             sub_dic[j] = 0
-        R[i] = sub_dic       
+        R[i] = sub_dic
+
+    # fill the rewards in
     for i in range(len(A)):
         R[A[i]][Z[i]] = weight[i]
+
     return R
 
 def initial_Q(R):
-    seed = np.random.randint(0, 100)
+    # seed = np.random.randint(0, 100)
+    # same shape as R
     Q = copy.deepcopy(R)
     for i in Q.keys():
         for j in Q[i].keys():
+            # initialized Q
             # Q[i][j] = rand_number(seed)
             Q[i][j] = 0
     return Q
